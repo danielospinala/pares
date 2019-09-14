cce794b9502e4c6d4054d0d7f15e75f13766f4c3
danielospina@gmail.com <danielospina@gmail.com>
2019-09-13 22:13:51 -0500

    visit1

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const express = require('express');
 const app = express();
 
+var mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e) { console.error(e); });
+
+// definimos el schema
+var schema = mongoose.Schema({
+date: { type: Date, default: Date.now  },
+name: { type: String, default: "Anonimo"  },
+
+});
+// definimos el modelo
+var Visitor = mongoose.model("Visitor", schema);
+
+app.get('/', (req, res) => {
+
+var first = new Visitor({ name: req.query.name });
+first.save(function(err) {
+if (err) return console.error(err);
+});
+
+res.send('<h1>El vistante fue almacenado con exito!</h1>');
+
+
+});
+
+
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
+/*
+const express = require('express');
+const app = express();
+
 app.get('/', (req, res) => {
 
 var str ='';
@@ -14,3 +47,4 @@ for (var i = 1; i <= 50; i++) {
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
+*/
