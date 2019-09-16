2a0d4929b077f4a7d69625b467527bdf5bc98667
danielospina@gmail.com <danielospina@gmail.com>
2019-09-16 18:56:07 -0500

    visit5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 var mongoose = require("mongoose");
-//mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 
@@ -31,22 +31,3 @@ res.send('<h1>El vistante fue almacenado con exito</h1>');
 
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
-
-/*
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-
-var str ='';
-for (var i = 1; i <= 50; i++) {
-  if (i % 2 == 0) {
-    str += "<p>" + i + " Soy Par! </p>";
-  }else {
-    str += "<p>" + i + " Soy Impar! </p>";
-  }
-}res.send(str);
-});
-
-app.listen(3000, () => console.log('Listening on port 3000!'));
-*/
