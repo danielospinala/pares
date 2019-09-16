2e9c5d0f2299f9aa875331829b8bada4348e2e51
danielospina@gmail.com <danielospina@gmail.com>
2019-09-16 18:50:15 -0500

    visit4

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 
 var mongoose = require("mongoose");
 //mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true });
-mongoose.connect('mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
 mongoose.connection.on("error", function(e) { console.error(e); });
 
 // definimos el schema
